dba2ca63a76dc7d2d07ab4e55ef5f6200ecff1d9
Rudiethenomad <111630606+Rudiethenomad@users.noreply.github.com>
2022-10-09 12:14:12 +0200

    Restart

---
+++ thw.js
@@ -0,0 +1,66 @@
+
+/* console.log (/n..........Exersize1.........)
+function area (p11,p12) { 
+    const triangle ="triangle area," + (11 * 12/2);
+return triangle;
+
+}
+console.log(area(2,3))
+
+/* console.log ("Exersize 2")
+
+
+function crazySum (p5,p4)
+{ if (p5 === p4){
+    console.log ((p4 + p5) *3);
+} else { console.log (p5 + p4);}
+  
+      
+}
+crazySum (5,4);
+crazySum (5,5);
+crazySum (3,3);
+crazySum(3,6);
+crazySum (2,2)
+*/
+
+// console.log ("Exersise 3");
+
+function crazyDiff (p1, p2)
+
+{
+
+
+return Math.abs(p1-p2);
+        
+
+// console.log (crazyDiff (p1,'any', p2, '19'))
+}
+
+
+crazyDiff (10,5);
+
+//     if (p11*p19 > 19){
+//     console.log ((p11 + p19) *3);
+// } else { console.log (p11 + p19);}
+  
+      
+
+// {  if (Math.abs(p11, p19) > 19){
+//     console.log (Math.abs(p11, p19 * 3));
+// } else {Math.abs (p11, p19);}
+
+
+    // {if (x>19){console.log(x*19);}}
+    
+
+
+
+
+// crazyDiff (11?,19)
+
+
+
+
+
+
